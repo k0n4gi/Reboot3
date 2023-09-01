718ae266a8d8d3b2fb033d3c62e6f66b8d2c910f
joonsig <bestknow98@naver.com>
2023-09-01 12:14:36 +0900

    Update file system dump

--- Mac/Apple_APFS.py
+++ Mac/Apple_APFS.py
@@ -1,16 +1,12 @@
+# 코드를 진행하기 전 해결 사항
+# 1. 시스템 환경설정
+# 2. 보안 및 개인 정보 보호
+# 3. 전체 디스크 접근 권한
+# 4. Terminal 관련 앱 권한 승인
+# 5. diskutil list 명령어로 APFS 디스크 찾은 후 해당 APFS dump 진행
+# 6. 보통 APFS는 /dev/disk0s2 입니다.
+
 import subprocess
-import re
-
-def find_apfs_partition():
-    cmd_output = subprocess.check_output(['diskutil', 'list']).decode('utf-8')
-    
-    # diskutil list의 출력에서 Apple_APFS 파티션을 찾는 정규 표현식 사용
-    matches = re.findall(r'(/dev/disk\d+s\d+)\s+Apple_APFS', cmd_output)
-
-    if matches:
-        return matches[0]
-    else:
-        return None
 
 def dump_partition(partition, output_file="APPLE_APFS.raw"):
     # dd 명령어를 사용하여 파티션을 덤프
@@ -18,14 +14,14 @@ def dump_partition(partition, output_file="APPLE_APFS.raw"):
     subprocess.run(cmd)
 
 def main():
-    partition = find_apfs_partition()
+    partition = input("Please enter the APFS partition identifier (e.g., /dev/disk0s2): ")
     
     if partition:
-        print(f"Found APFS partition: {partition}. Dumping to dump.raw...")
+        print(f"Dumping {partition} to APPLE_APFS.raw...")
         dump_partition(partition)
         print("Dump complete.")
     else:
-        print("APFS partition not found.")
+        print("Invalid partition identifier.")
 
 if __name__ == "__main__":
     main()
