153fbd52cc13deb844bba9d522732b22106c073c
buaii <129252044+buaii@users.noreply.github.com>
2023-09-01 19:02:15 +0900

    Update reboot3.py
    1. LNK
    2. Powershell log
    3. Memory Dump
    4. RecycleBin
    5. Browser history
    6. registry
    7. UserAssist
    8. AutoRun

--- Windows/reboot3.py
+++ Windows/reboot3.py
@@ -7,6 +7,7 @@ from guppy import hpy
 import sqlite3
 import subprocess
 import winreg
+import base64
 
 def init():
 	f = Figlet(font='slant')
@@ -16,6 +17,7 @@ def init():
 def showOptions():
 	print("\n==========Options==========")
 	print("1) Memory Dump")
+	print("20) UserAssist")
 	print("22) Registry")
 	print("23) Internet Browser History")
 	print("24) Recycle Bin")
@@ -30,7 +32,7 @@ def memoryDump():
 
 	# "MemoryDump" 폴더 생성 후 저장
 	if not os.path.exists(destination_folder):
-        	os.makedirs(destination_folder)
+		os.makedirs(destination_folder)
 	
 	# 메모리 덤프 파일 경로
 	memory_dump_file = os.path.join(destination_folder, 'memory_dump.txt')
@@ -39,6 +41,42 @@ def memoryDump():
 	with open(memory_dump_file, "w") as f:
 		f.write(str(h.heap()))
 
+
+def userAssist():
+	print("\n==========UserAssist File==========")
+	output_folder = os.path.join(os.getcwd(), "UserAssist")
+	userassist_key_path = r"Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist"
+
+	# "MemoryDump" 폴더 생성 후 저장
+	if not os.path.exists(output_folder):
+		os.makedirs(output_folder)
+
+	try:
+		with winreg.OpenKey(winreg.HKEY_CURRENT_USER, userassist_key_path, 0, winreg.KEY_READ | winreg.KEY_WOW64_64KEY) as key:
+			userAssistsRegistry(output_folder, key)
+			print(f"UserAssist data has been saved to {output_folder}")
+	except Exception as e:
+		print(f"Error: {e}")
+
+
+def userAssistsRegistry(output_folder, registry_key):
+    try:
+        with open(output_folder, 'w') as file:
+            subkey_count, _, _ = winreg.QueryInfoKey(registry_key)
+            for i in range(subkey_count):
+                subkey_name = winreg.EnumKey(registry_key, i)
+                subkey_path = f"{output_folder}_{subkey_name}.txt"
+                subkey = winreg.OpenKey(registry_key, subkey_name)
+                file.write(f"Subkey: {subkey_name}\n")
+                subkey_count, _, _ = winreg.QueryInfoKey(subkey)
+                for j in range(subkey_count):
+                    value_name, value_data, _ = winreg.EnumValue(subkey, j)
+                    file.write(f"  {value_name}: {value_data}\n")
+                file.write("\n")
+                userAssistsRegistry(subkey, subkey_path)
+    except Exception as e:
+        print(f"Error: {e}")
+
 def registry():
 	print("\n==========Registry File==========")
 	current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -46,8 +84,8 @@ def registry():
 
 	# "MemoryDump" 폴더 생성 후 저장
 	if not os.path.exists(destination_folder):
-        	os.makedirs(destination_folder)
-    # 레지스트리 키 및 값 읽어오기
+		os.makedirs(destination_folder)
+        # 레지스트리 키 및 값 읽어오기
 	with open(os.path.join(destination_folder, 'full_registry_backup.reg'), 'w', encoding='utf-16') as reg_file:
 		reg_file.write('Windows Registry Editor Version 5.00\n\n')
 		export_registry_key(reg_file, winreg.HKEY_LOCAL_MACHINE, '')
@@ -82,11 +120,10 @@ def save_browser_history():
 
 	# "MemoryDump" 폴더 생성 후 저장
 	if not os.path.exists(destination_folder):
-        	os.makedirs(destination_folder)
+		os.makedirs(destination_folder)
 
 	# 크롬 브라우저의 기록 데이터베이스 경로
 	chrome_history_path = os.path.expanduser('~') + '\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History'
-
 	# 복사할 임시 파일 경로
 	temp_db_path = os.path.join(destination_folder, 'temp_history')
 
@@ -104,10 +141,10 @@ def save_browser_history():
 	# 파일로 저장
 	history_file_path = os.path.join(destination_folder, 'browser_history.txt')
 	with open(history_file_path, 'w', encoding='utf-8') as file:
-	    for item in history:
-	        title, url, timestamp = item
-        	time_formatted = str(timestamp)
-	        file.write(f"Title: {title}\nURL: {url}\nTimestamp: {time_formatted}\n\n")
+		for item in history:
+			title, url, timestamp = item
+			time_formatted = str(timestamp)
+			file.write(f"Title: {title}\nURL: {url}\nTimestamp: {time_formatted}\n\n")
 
 	# 연결 종료 및 임시 데이터베이스 파일 삭제
 	cursor.close()
@@ -122,15 +159,15 @@ def recycleBin():
 	# 'RecycleBin' 폴더 생성
 	destination_folder = os.path.join(os.getcwd(), 'RecycleBin')
 	if not os.path.exists(destination_folder):
-	    os.makedirs(destination_folder)
+		os.makedirs(destination_folder)
 
 	try:
 	    # Powershell 스크립트 실행
-	    script_path = os.path.join(os.path.dirname(__file__), 'copy_recycle.ps1')
-	    command = ["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path]
-	    subprocess.run(command, shell=True, check=True)
+		script_path = os.path.join(os.path.dirname(__file__), 'copy_recycle.ps1')
+		command = ["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path]
+		subprocess.run(command, shell=True, check=True)
 
-	    print("휴지통 내용이 'RecycleBin' 폴더에 복사되었습니다.")
+		print("휴지통 내용이 'RecycleBin' 폴더에 복사되었습니다.")
 	except Exception as e:
 	    print(f"오류가 발생했습니다: {e}")
 
@@ -140,21 +177,22 @@ def lnkFile():
 	current_directory = os.path.dirname(os.path.abspath(__file__))
 	destination_folder = os.path.join(current_directory, "LNK")
 	if not os.path.exists(destination_folder):
-	    os.makedirs(destination_folder)
+		os.makedirs(destination_folder)
 
 	# .lnk파일 위치
 	source_folder = os.path.expanduser("~") + "\\AppData\\Roaming\\Microsoft\\Windows\\Recent"
 
 	# file copy
 	for root, dirs, files in os.walk(source_folder):
-	    for file in files:
-	        source_file_path = os.path.join(root, file)
-	        destination_file_path = os.path.join(destination_folder, file)
-	        try:
-	            shutil.copy2(source_file_path, destination_file_path)
-	            print(f"복사 완료: {file}")
-	        except Exception as e:
-	            print(f"복사 실패: {file}, 오류: {e}")
+		for file in files:
+			source_file_path = os.path.join(root, file)
+			destination_file_path = os.path.join(destination_folder, file)	
+
+			try:
+				shutil.copy2(source_file_path, destination_file_path)
+				print(f"복사 완료: {file}")
+			except Exception as e:
+				print(f"복사 실패: {file}, 오류: {e}")
 			
 def PowerShellLogFile():
 	print("\n==========PowerShell Log File==========")
@@ -162,21 +200,21 @@ def PowerShellLogFile():
 	current_directory = os.path.dirname(os.path.abspath(__file__))
 	destination_folder = os.path.join(current_directory, "PowerShellLog")
 	if not os.path.exists(destination_folder):
-	    os.makedirs(destination_folder)
+		os.makedirs(destination_folder)
 
 	# .lnk파일 위치
 	source_folder = os.path.expanduser("~") + "\\AppData\\Roaming\\Microsoft\\Windows\\PowerShell\\PSReadLine"
 
 	# file copy
 	for root, dirs, files in os.walk(source_folder):
-	    for file in files:
-	        source_file_path = os.path.join(root, file)
-	        destination_file_path = os.path.join(destination_folder, file)
-	        try:
-	            shutil.copy2(source_file_path, destination_file_path)
-	            print(f"복사 완료: {file}")
-	        except Exception as e:
-	            print(f"복사 실패: {file}, 오류: {e}")
+		for file in files:
+			source_file_path = os.path.join(root, file)
+			destination_file_path = os.path.join(destination_folder, file)
+			try:
+				shutil.copy2(source_file_path, destination_file_path)
+				print(f"복사 완료: {file}")
+			except Exception as e:
+				print(f"복사 실패: {file}, 오류: {e}")
 
 def main():
 	init()
@@ -189,6 +227,8 @@ def main():
 			exit()
 		elif options == "1":
 			memoryDump()
+		elif options == "20":
+			userAssist()
 		elif options == "22":
 			registry()
 		elif options == "23":
